efa29cb448d2ff5a95284a1973c60374fb1079ad
Documenter.jl <documenter@juliadocs.github.io>
2021-12-03 19:02:56 +0000

    build based on 958c8dd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JuKeBOX","category":"page"},{"location":"#JuKeBOX","page":"Home","title":"JuKeBOX","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JuKeBOX.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JuKeBOX]","category":"page"}]
+}
