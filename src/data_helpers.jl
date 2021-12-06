f66a87dc07e6913326b4269dc1e78353156261ee
danielpalumbo <daniel.palumbo@cfa.harvard.edu>
2021-12-06 09:57:24 -0500

    Added syserr functions for logcamp.

--- src/data_helpers.jl
+++ src/data_helpers.jl
@@ -4,15 +4,24 @@ using PyCall
 
 
 
-function amp_debias(amp, σ, force_nonzero=False)
+function amp_debias(amp, σ; force_nonzero=false)
     amp = abs(amp)
     σ = abs(σ)
-    deb² = amp^2 - σ^2
-    deb²[amp.<σ].=0 
-    if force_nonzero
-        deb²[amp.<σ] = σ^2
+    if amp < σ
+        if force_nonzero
+            deb² = σ^2
+        else
+            deb² = 0
+        end
+    else
+        deb² = amp^2 - σ^2
     end
     return √deb²
+end
+
+function logcamp_debias(log_camp, snr1, snr2, snr3, snr4)
+    log_camp_debias = log_camp + 0.5*(1/snr1^2 + 1/snr2^2 - 1/snr3^2 -1/snr4^2)
+    return log_camp_debias
 end
 
 
@@ -22,7 +31,7 @@ end
     the definitions of numerators and denominators
     will work out.
 """
-function make_log_closure_amplitude(n1amp, n2amp, d1amp, d2amp, n1err, n2err, d1err, d2err, debias=true)
+function make_log_closure_amplitude(n1amp, n2amp, d1amp, d2amp, n1err, n2err, d1err, d2err; debias=true)
     p1 = abs(n1amp)
     p2 = abs(n2amp)
     p3 = abs(d1amp)
@@ -42,12 +51,21 @@ function make_log_closure_amplitude(n1amp, n2amp, d1amp, d2amp, n1err, n2err, d1
     logcamp = log(p1)+log(p2)-log(p3)-log(p4)
     logcamp_err = √(1/snr1^2 + 1/snr2^2 + 1/snr3^2 + 1/snr4^2)
     if debias
-        #WIP
+        logcamp = logcamp_debias(logcamp, snr1, snr2, snr3, snr4)
     end
+    return logcamp, logcamp_err
 end
 
-function amp_add_syserr(amp, amp_error, fractional=0, additive=0)
+function amp_add_syserr(amp, amp_error; fractional=0, additive=0)
     σ = √(amp_error^2+(fractional*amp)^2+additive^2)
-    return σ
+    return amp, σ
+end
+
+function logcamp_add_syserr(n1amp, n2amp, d1amp, d2amp, n1err, n2err, d1err, d2err; fractional=0, additive = 0, debias=true)
+    n1amp, n1err = amp_add_syserr(n1amp, n1err, fractional=fractional, additive=additive)
+    n2amp, n2err = amp_add_syserr(n2amp, n2err, fractional=fractional, additive=additive)
+    d1amp, d1err = amp_add_syserr(d1amp, d1err, fractional=fractional, additive=additive)
+    d2amp, d2err = amp_add_syserr(d2amp, d2err, fractional=fractional, additive=additive)
+    return make_log_closure_amplitude(n1amp, n2amp, d1amp, d2amp, n1err, n2err, d1err, d2err, debias=debias)
 end
 
